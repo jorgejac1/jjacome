5dbbfe7ab0d3a3907da9ae612ac91142507eed81
Jorge Jacome <jorge2002_mx@hotmail.com>
2019-09-04 22:24:38 -0400

    feat(home): First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Home from './components/home/home';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default App;
---
+++ src/components/home/home.js
@@ -0,0 +1,66 @@
+import React, { Component, useState, useEffect } from "react";
+import Typist from "react-typist";
+import "./home.css";
+
+function Home() {
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    setCount(1);
+  }, [count]);
+
+  return (
+    <div id="header" className="home">
+      <div className="container">
+        <div className="header-content">
+          <h1>I'm a</h1>
+          {count ? (
+            <Typist avgTypingDelay={100} onTypingDone={() => setCount(0)}>
+              <span className="green">Developer</span>
+              <Typist.Backspace count={10} delay={2000} />
+              <span>Manager</span>
+              <Typist.Backspace count={10} delay={2000} />
+              <span className="red">Leader</span>
+              <Typist.Backspace count={10} delay={2000} />
+              <span className="green">ME</span>
+              <span>XIC</span>
+              <span className="red">AN</span>
+              <Typist.Backspace count={10} delay={2000} />
+            </Typist>
+          ) : (
+            ""
+          )}
+          <ul className="list-unstyled list-social">
+            <li>
+              <a href="#">
+                <i className="fab fa-facebook fa-2x"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-twitter fa-2x"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-instagram fa-2x"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-linkedin fa-2x"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-github fa-2x"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
